78bbc5a437c99eaf507b429177e2cd73b24e0b70
Pramod Kumar <pramodkumar.damam73@gmail.com>
2017-05-10 20:48:12 +0530

    Add test cases

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,13 +1,37 @@
 import { expect } from "chai";
 import { slow, suite, test, timeout } from "mocha-typescript";
+import * as mongoose from "mongoose";
 import BookRepo, { Book } from "./models/Book";
 import UserRepo, { User } from "./models/User";
+
+(mongoose as any).Promise = global.Promise;
 
 @suite
 class Tests {
 
-    @test
-    public connection() {
-        const expected = expect(true).to.be.ok;
+    @test(slow(1000), timeout(5000))
+    public connection(done) {
+        mongoose.connect("mongodb://localhost/test", (error) => {
+            const e = expect(error).to.be.not.ok;
+            done();
+        });
     }
+
+    @test
+    public async insert() {
+        return UserRepo.create({ name: "Test" });
+    }
+
+    @test
+    public async query() {
+        return UserRepo.query.find({});
+    }
+
+    @test
+    public async accessMethod() {
+        const user = await UserRepo.query.findOne({});
+        const name = user.getName();
+        const e = expect(name).to.equal("Test");
+    }
+
 }
