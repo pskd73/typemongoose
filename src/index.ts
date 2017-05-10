e79091d83e2ccfd9bdc09b057d613e80f5d59981
Pramod Kumar <pramodkumar.damam73@gmail.com>
2017-05-10 15:13:10 +0530

    Expose library

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
-import * as mongoose from "mongoose";
-import BookRepo, { Book } from "./test/models/Book";
-import UserRepo, { User } from "./test/models/User";
+import * as BaseModel from "./BaseModel";
+import * as Decorators from "./Decorators";
+import * as ModelRepo from "./ModelRepo";
 
-const init = async () => {
-    console.log("connecting..");
-    (mongoose as any).Promise = global.Promise;
-    const connectionResponse = await mongoose.connect("mongodb://localhost/typemongoose");
-    console.log("connected.");
-
-    const allUsers = await UserRepo.query.find({});
-    console.log(allUsers);
-
-    const book = await BookRepo.query.findOne().populate("user");
-    const user = book.user as User;
-    console.log(user.getName());
+export {
+    BaseModel,
+    Decorators,
+    ModelRepo
 };
-
-init();
